c04699ed6363049b40894b03eab7911417a6d7e5
jiayuwinds <66798137@qq.com>
2013-08-10 15:07:51 +0800

    first commit newfx

---
+++ src/com/example/newfx/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.newfx;
+
+import org.apache.cordova.DroidGap;
+
+import android.os.Bundle;
+
+public class MainActivity extends DroidGap {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_main);
+        super.loadUrl("file:///android_asset/www/index.html");
+    }
+}
